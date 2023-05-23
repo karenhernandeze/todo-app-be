26a4d6e33e7728a0d998af7c0a69d7e4e3dfe973
karenhernandeze <karenaliciahernandez@hotmail.com>
2023-05-22 23:13:53 -0600

    changed names

--- src/main/java/karenhernandeze/demo/ToDoApplication.java
+++ src/main/java/karenhernandeze/demo/ToDoApplication.java
@@ -2,12 +2,13 @@ package karenhernandeze.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ToDoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ToDoApplication.class, args);
 	}
-
 }
